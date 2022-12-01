63c8a1ffc315765b276ce44862e19873a8cc1c02
Hyden Polikoff <hyden.dev@gmail.com>
2022-12-01 09:56:01 -0700

    move the graph steps separate file

---
+++ graph_steps.py
@@ -0,0 +1,42 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from conjugate_gradient import conjugate_gradient_2
+
+
+if __name__ == '__main__':
+
+    # Problem formulation
+    A = np.array([[4, 3, 0],
+                  [3, 4, -1],
+                  [0, -1, 4]])
+    b = np.array([[24],
+                  [30],
+                  [-24]])
+
+    x0 = np.array([[0, 0, 0]])
+    x0 = x0.T
+
+    err_code, x_star, steps, iters, proc_time = conjugate_gradient_2(A, b, 1e-10, 1_000)
+    print(x_star)
+    print(iters)
+
+    # plotting the steps
+    xs = [x[0] for x in steps]
+    ys = [y[1] for y in steps]
+    zs = [y[2] for y in steps]
+
+    fig = plt.figure(figsize=(12, 12), dpi=400)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(xs, ys, zs, marker='x', linestyle='--', color='red')
+
+    for x, y, z in zip(xs, ys, zs):
+        label = f'({x:.2f}, {y:.2f}, {z:.2f})'
+        ax.text(x, y, z, label)
+
+    ax.set_title("CG Steps")
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_zlabel('x3')
+    # plt.show()
+    plt.savefig('3d-plot.png', bbox_inches='tight')
